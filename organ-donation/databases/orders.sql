06cb631277d716f18ceb782b0e6997033fbd41e6
Chandana <83870380+ChandanaVaidya17@users.noreply.github.com>
2023-01-18 21:04:40 +0530

    Update orders.sql

--- organ-donation/databases/orders.sql
+++ organ-donation/databases/orders.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 11, 2023 at 12:52 PM
+-- Generation Time: Jan 18, 2023 at 04:26 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.19
 
@@ -28,19 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `orders` (
-  `hosp_name` varchar(30) NOT NULL,
-  `address` varchar(30) NOT NULL,
-  `ph_no` int(13) NOT NULL,
-  `date` varchar(11) NOT NULL,
-  `organ_id` int(5) NOT NULL
+  `hosp_name` varchar(40) DEFAULT NULL,
+  `adress` varchar(50) DEFAULT NULL,
+  `ph_no` int(20) DEFAULT NULL,
+  `date` varchar(10) DEFAULT NULL,
+  `organ_id` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`hosp_name`, `address`, `ph_no`, `date`, `organ_id`) VALUES
-('rmv', 'banglore', 1023654897, '11-1-2023', 103);
+INSERT INTO `orders` (`hosp_name`, `adress`, `ph_no`, `date`, `organ_id`) VALUES
+('manipal', 'banglore', 2147483647, '11-1-2023', '103'),
+('rmv', 'banglore', 2147483647, '21-01-2023', '106');
 
 --
 -- Indexes for dumped tables
@@ -51,6 +52,16 @@ INSERT INTO `orders` (`hosp_name`, `address`, `ph_no`, `date`, `organ_id`) VALUE
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`organ_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`organ_id`) REFERENCES `trigger1` (`organ_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
