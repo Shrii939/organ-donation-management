9f16064dfc528ff602c8d40b8452bb2afc69a249
Chandana <83870380+ChandanaVaidya17@users.noreply.github.com>
2023-01-18 21:05:41 +0530

    Update organ_bank.sql

--- organ-donation/databases/organ_bank.sql
+++ organ-donation/databases/organ_bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 11, 2023 at 12:52 PM
+-- Generation Time: Jan 18, 2023 at 04:26 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.19
 
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `organ_bank` (
-  `organ_id` int(5) NOT NULL,
+  `organ_id` varchar(5) NOT NULL,
   `test_details` varchar(50) DEFAULT NULL,
   `issues` varchar(50) DEFAULT NULL,
-  `org_type` varchar(10) DEFAULT NULL
+  `org_type` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -39,15 +39,21 @@ CREATE TABLE `organ_bank` (
 --
 
 INSERT INTO `organ_bank` (`organ_id`, `test_details`, `issues`, `org_type`) VALUES
-(101, 'passed', 'ok', 'kidney'),
-(105, 'tested ok', 'no issues', 'heart'),
-(202, 'passed', 'no', 'brain');
+('101', 'passed', 'minor issue', 'eye'),
+('101', 'passed', 'no issuues', 'kidney'),
+('102', 'passed', 'no issues', 'eye'),
+('103', 'passed', 'ok\r\n', 'kidney'),
+('103', 'passed', 'noo issues', 'liver'),
+('104', 'passed', 'no issues', 'eye'),
+('104', 'passed', 'no', 'kidney'),
+('105', 'passed', 'ok', 'liver'),
+('106', 'passed', 'no', 'brain');
 
 --
 -- Triggers `organ_bank`
 --
 DELIMITER $$
-CREATE TRIGGER `available` AFTER INSERT ON `organ_bank` FOR EACH ROW insert into trigger1 VALUES(new.organ_id,new.test_details,new.org_type,NOW())
+CREATE TRIGGER `available` AFTER INSERT ON `organ_bank` FOR EACH ROW insert into trigger1 values(new.organ_id,new.test_details,new.org_type,now())
 $$
 DELIMITER ;
 
@@ -59,17 +65,7 @@ DELIMITER ;
 -- Indexes for table `organ_bank`
 --
 ALTER TABLE `organ_bank`
-  ADD PRIMARY KEY (`organ_id`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `organ_bank`
---
-ALTER TABLE `organ_bank`
-  ADD CONSTRAINT `organ_bank_ibfk_1` FOREIGN KEY (`organ_id`) REFERENCES `organ` (`organ_id`) ON DELETE CASCADE;
+  ADD PRIMARY KEY (`organ_id`,`org_type`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
